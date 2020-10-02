331571697a84151d6b7a9e34d69a9ab8d59fb2f5
chernataly2020 <69033364+chernataly2020@users.noreply.github.com>
2020-10-02 19:22:20 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -21,19 +21,13 @@ exports.max = function max(array) {
 
 exports.avg = function avg(array) {
     let objIsArray = Array.isArray(array);
-    console.log(array.length);
-    console.log(objIsArray);
     if (array.length == 0 || objIsArray == false || undefined) {
-        console.log(0);
         return 0;
     } else {
       let sum=0;
       for(let i=1;i<array.length;i++){
         sum  += array[i];
       }
-      console.log(sum);
-      console.log(sum / array.length);
       return sum / array.length;
-    //  return Math.max.apply( Math, array );
     }
 }
