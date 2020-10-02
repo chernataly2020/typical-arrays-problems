115896ed60eb0da4aa1041c2f400309dc2182663
chernataly2020 <69033364+chernataly2020@users.noreply.github.com>
2020-10-02 19:21:11 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
     let objIsArray = Array.isArray(array);
-    if (array.length == 0 || objIsArray == false) {
+    if (array.length == 0 || objIsArray == false || undefined) {
         return 0;
-    }
-    else {
-        return Math.min(array);
+    } else {
+        return Math.min.apply( Math, array );
     }
 }
 
-exports.max = function max (array) {
+
+
+exports.max = function max(array) {
     let objIsArray = Array.isArray(array);
-    if (array.length == 0 || objIsArray == false) {
+    if (array.length == 0 || objIsArray == false || undefined) {
         return 0;
-    }
-    else {    
-      return Math.max(array);
+    } else {
+        return Math.max.apply( Math, array );
     }
 }
 
-exports.avg = function avg (array) {
-  let objIsArray = Array.isArray(array);
-  if (array.length == 0 || objIsArray == false) {
+exports.avg = function avg(array) {
+    let objIsArray = Array.isArray(array);
+    console.log(array.length);
+    console.log(objIsArray);
+    if (array.length == 0 || objIsArray == false || undefined) {
+        console.log(0);
         return 0;
-    }
-    else {
+    } else {
       let sum=0;
       for(let i=1;i<array.length;i++){
         sum  += array[i];
-        }
+      }
+      console.log(sum);
+      console.log(sum / array.length);
       return sum / array.length;
+    //  return Math.max.apply( Math, array );
     }
 }
