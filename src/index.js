4092e280b6b1e46098c89a1cdf3c0655befdac1d
chernataly2020 <cherkesnataly@gmail.com>
2020-10-03 01:18:22 +0300

    commit message

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-
 exports.min = function min(array) {
     let objIsArray = Array.isArray(array);
-    if (objIsArray === false || undefined){
+    if (objIsArray === false || undefined) {
         return 0;
-    } else if  (array.length === 0) {
+    } else if (array.length === 0) {
         return 0;
     } else {
-        return Math.min.apply( Math, array );
+        return Math.min.apply(Math, array);
     }
 }
 
 exports.max = function max(array) {
     let objIsArray = Array.isArray(array);
-    if (objIsArray === false || undefined){
+    if (objIsArray === false || undefined) {
         return 0;
-    } else if  (array.length === 0) 
-    {
+    } else if (array.length === 0) {
         return 0;
     } else {
-        return Math.max.apply( Math, array );
+        return Math.max.apply(Math, array);
     }
 }
 
 exports.avg = function avg(array) {
     let objIsArray = Array.isArray(array);
-    if (objIsArray === false || undefined){
+    if (objIsArray === false || undefined) {
         return 0;
-    } else if  (array.length === 0) 
-    {
+    } else if (array.length === 0) {
         return 0;
-    } else{
-      let sum=0;
-      for(let i=1;i<array.length;i++){
-        sum  += array[i];
-      }
-      return sum / array.length;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
     }
 }
